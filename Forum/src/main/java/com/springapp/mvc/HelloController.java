590c7b1d35a846408907112c065cf5842e945639
Angrytree <steainer@gmail.com>
2015-05-06 23:15:32 +0300

    init

---
+++ Forum/src/main/java/com/springapp/mvc/HelloController.java
@@ -0,0 +1,28 @@
+package com.springapp.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+	@RequestMapping(method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+		model.addAttribute("message", "Hello world!");
+		return "hello";
+	}
+
+	@RequestMapping(value = "/send", method = RequestMethod.POST)
+	public String addUser(@RequestParam("data") String data,ModelMap model) {
+
+		model.addAttribute("data",data);
+
+		return "sendData";
+	}
+
+}
---
+++ Forum/target/Forum/WEB-INF/pages/sendData.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Администратор
+  Date: 06.05.2015
+  Time: 22:30
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+  Hello2
+  <h2>${data}</h2>
+</body>
+</html>
